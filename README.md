7083e96d761a3f66c6c2a4e831bd00609fb0d30d
João Cruz <jonasus123@hotmail.com>
2025-03-18 18:09:04 +0000

    rr a tudo

--- README.md
+++
@@ -1,3 +0,0 @@
-# FelixBus
-Pretende-se o desenvolvimento de uma aplicação que permita a gestão de um “web site para  gestão de viagens de autocarro: FelixBus”, com uma implementação em PHP. 
-Teste inicial.
--- baseDeDados/basedados.php
+++ baseDeDados/basedados.php
@@ -1,15 +1,15 @@
 <?php
+// Definindo as configurações de conexão
 $host = 'localhost';
-$dbname = 'felixbus';
-$username = 'root';
-$password = '';
+$dbname = 'felixbus';  // Nome da base de dados
+$username = 'root';  // Nome de utilizador
+$password_db = '';   // Senha do banco de dados
 
 // Criação da conexão MySQLi
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password_db, $dbname);
 
 // Verifica se houve erro na conexão
 if ($conn->connect_error) {
     die("Erro ao conectar à base de dados: " . $conn->connect_error);
 }
-
 ?>
